990252d552ccd36a0d8b3858eb92399690119e90
Victor Scopel <victorscopel@hotmail.com>
2021-12-14 17:25:55 -0400

    Update bfc.user.js
    Atualizado para v2.1

--- bfc.user.js
+++ bfc.user.js
@@ -3,12 +3,11 @@
 // @namespace   https://github.com/victorscopel/melhoriasbfc
 // @homepage    https://github.com/victorscopel/melhoriasbfc
 // @supportURL  https://github.com/victorscopel/melhoriasbfc/issues
-// @version     1.1
+// @version     1.2
 // @description Melhorias para o site do bfc
 // @author      Victor Scopel
-// @match       https://blockfarm.club/farm/*
-// @icon        https://www.google.com/s2/favicons?domain=blockfarm.club
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @match       https://blockfarmclub.com/game/farm/*
+// @icon        https://www.google.com/s2/favicons?domain=blockfarmclub.com
 // @updateURL   https://raw.githubusercontent.com/victorscopel/melhoriasbfc/main/bfc.user.js
 // @downloadURL https://raw.githubusercontent.com/victorscopel/melhoriasbfc/main/bfc.user.js
 // @grant GM_xmlhttpRequest
@@ -20,18 +19,21 @@
     'use strict';
     // CONVERTE O HORÁRIO DE UTC PARA O HORÁRIO LOCAL DO SEU COMPUTADOR
     var horario = "";
-     $('[id*="clock"]').each(function() {
-         horario = $(this).text().replace('Harvest on : ', '').replace(' UTC','').replace(' ',',').replace('-',',').replace('-',',').replace(':',',').replace(':',',');
+     $('span.animate-pulse.text-pink-300').each(function() {
+         horario = $(this).text().replace('Harvest on: ', '').replace(' UTC','').replace(' ',',').replace('-',',').replace('-',',').replace(':',',').replace(':',',');
          var array = horario.split(','),
          ano = array[0], mes = array[1], dia = array[2], hora = array[3], minuto = array[4], segundo = array[5];
+         console.log(array);
          let date = new Date(Date.UTC(ano, mes-1, dia, hora, minuto, segundo));
          horario = date.toLocaleString('pt-BR');
          $(this).text("Colher em: "+horario);
     });
+/*
 // DEIXA LANDS QUE NÃO SÃO LENDARIAS UM POUCO MAIS APAGADAS PRA FACILITAR ACHAR AS LANDS LENDARIAS
 if (window.location.href.indexOf("mapview") > -1) {
      $(".relative td").each(function() {
         $('i:not(:contains("Legendary"))').closest(this).find("button").css("opacity","0.2");
     });
 }
+*/
 })();
